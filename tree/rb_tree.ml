5301ae9f6d798b4a7ccfbae444b22729b167c917
Beloshapko <vasasasa708@gmail/com>
2020-11-30 11:29:30 +0300

    CW

--- tree/rb_tree.ml
+++ tree/rb_tree.ml
@@ -1,12 +1,23 @@
-type 'a tree = Leaf | BlackNode of ('a tree) * ('a tree) * 'a | RedNode of ('a tree) * ('a tree) * 'a
+
+type color = Red | Black
+type 'a tree = Leaf | Node of ('a tree) * ('a tree) * 'a * color
 
 let rec mem t n = 
     match t with 
        Leaf -> false
-     | RedNode (l,r,x) -> if (n = x) then true else if (n > x) then mem r n else mem l n
-     | BlackNode (l,r,x) -> if (n = x) then true else if (n > x) then mem r n else mem l n;;
+     | Node (l,r,x,c) -> if (n = x) then true else if (n > x) then mem r n else mem l n;;
+  
+let rotate t =
+    match t with 
 
-let rec add t n = 
-    Leaf -> RedNode(Leaf, Leaf, n)
-  | BlackNode (l, r, x) -> if (n = x) then t else if (n < x) then RedNode ((add l n), r, x) else RedNode (l, (add r n), x)
-  | RedNode ->
+
+let rec treeCicle t = 
+    match t with
+        Node (((Node lson, (RedNode rson, x), Red)), Black) -> RedNode (BlackNode lson, Node rson, b, x)
+      | BlackNode ((RedNode (RedNode dcare), r2, x2), r3, x3) -> BlackNode (RedNode dcare, RedNode (r2, r3), x2)
+
+let rec add t n =
+    match t with 
+        Leaf -> Node(Leaf, Leaf, n, Red)
+      | Node (l, r, x, c) -> if (n = x) then t else if (n < x) then treeCicle (Node (((add l n),r,n),Black)) else treeCicle (Node ((l,(add r n),n),Black))
+ 
