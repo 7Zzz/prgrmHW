c8c79acb24f0c7bd568a5ba0399532616b4e3a8f
7Zzz <m.miakishev@gmail.com>
2020-12-15 17:05:21 +0300

    added: make_tree_min_max, min_max_depth. Task 25.11

--- tree/balance_tree.ml
+++ tree/balance_tree.ml
@@ -13,6 +13,24 @@ let rec is_bamboo t =
 let rec rbamboo n = if n = 0 then Leaf else Node (Leaf, rbamboo (n-1), n-1);;
 let rec lbamboo n = if n = 0 then Leaf else Node (lbamboo (n-1),Leaf, n-1);;
 
+let rec min_max_depth t = 
+  match t with
+    Leaf -> (0,0)
+  | Node (l, r, _) -> let ((a,b), (c,d)) = (min_max_depth l, min_max_depth r) in 
+  ((min a c)+1, (max b d)+1);;
+
+
+
+let make_tree_min_max (min,max) = 
+  let rec f k = 
+    if k = 0 then Leaf else Node (f (k-1), f (k-1), 0) in 
+  let rec g t = 
+    match t with
+      Leaf -> f (max-min)
+     | Node (l,r,x) -> Node (l, g r, x) in
+  g (f min) ;;
+
+
 let is_balanced t = 
 	let rec f t = 
 		match t with
